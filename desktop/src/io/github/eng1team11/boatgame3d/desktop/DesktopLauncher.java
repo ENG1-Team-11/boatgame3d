5637fc496b8f81e1287d370faeb1ca31306952d1
AlexGarrity <garrity2799@gmail.com>
2020-11-12 15:34:13 +0000

    Stock libGDX project

---
+++ desktop/src/io/github/eng1team11/boatgame3d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.eng1team11.boatgame3d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.eng1team11.boatgame3d.BoatGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BoatGame(), config);
+	}
+}
